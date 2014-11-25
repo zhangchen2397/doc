8b3b92139d59188d5616494e339f93423a95e929
zhangchen <zhangchen2397@126.com>
2014-11-25 23:17:18 +0800

    modify shortcut.js

--- zc/shortcut/shortcut.js
+++ zc/shortcut/shortcut.js
@@ -17,7 +17,7 @@
         '#shortcut-pannel {display:none; z-index:9999; position:fixed; width:186px; height:76px; bottom:10px; background-color:#666666; opacity:0.9; border-radius:3px; }',
         '#shortcut-pannel .con { position:relative; height:76px; }',
         '#shortcut-pannel .ic-bg {background:transparent url(http://3gimg.qq.com/wap30/infoapp/touch/itravel/images/img/bg-ic.png) no-repeat scroll; -webkit-background-size: 20px auto;}',
-        '#shortcut-pannel .ic-closebtn {position:absolute; right:10px; top:10px; width:11px; height:11px; background-position:left top;}',
+        '#shortcut-pannel .ic-closebtn {position:absolute; right:10px; top:10px; z-index:3; width:11px; height:11px; background-position:left top;}',
         '#shortcut-pannel .tips {padding:15px 10px;}',
         '#shortcut-pannel .tips img {width:45px; border-radius:3px; float:left; margin-right:10px;}',
         '#shortcut-pannel .tips p {color:#FFFFFF; position:relative; top:-3px;}',
@@ -40,7 +40,7 @@
 
     var shortcut = function(config) {
         this.defaultConfig = {
-            icon: '',
+            icon: 'http://3gimg.qq.com/wap30/infoapp/touch/itravel/images/img_travel.png',
             key: 'travel',
             url: 'http://meiyou.qq.com',
             tips: '添加快捷方式，一键访问美游'
@@ -53,8 +53,13 @@
 
     $.extend(shortcut.prototype, {
         init: function() {
-            this._createDom();
-            this._initEvent();
+            if ( this._isShow() ) {
+                this._createDom();
+                this._initEvent();
+
+                //记录显示日期
+                localStorage.setItem('dailyShow', this.getFormatDate(new Date()));
+            }
         },
 
         _createDom: function() {
@@ -86,6 +91,9 @@
                 config = this.config;
 
             this.hide();
+
+            //记录主动关闭日期
+            localStorage.setItem('forceClose', this.getFormatDate(new Date()));
         },
 
         _initEvent: function() {
@@ -109,9 +117,35 @@
 
         _isShow: function() {
             var me = this,
-                config = this.config;
+                config = this.config,
+                oneDayMs = 1000 * 60 * 60 * 24,
+                ua = navigator.userAgent,
+                curDate = this.getFormatDate(new Date()),
+                lastShowDate = localStorage.getItem('dailyShow'),
+                forceCloseDate = localStorage.getItem('forceClose');
 
-            
+            //非ios下的safari不显示
+            if (!/iphone.*?safari/i.test(ua)) {
+                return false;
+            }
+
+            //如果已显示，且间隔时间小于一天，则不显示
+            if (lastShowDate) {
+                if (+new Date(curDate) - +new Date(lastShowDate) < oneDayMs) {
+                    return false;
+                }
+            }
+
+            //如果主动关闭过，且间隔时间少于7天，则不显示
+            if (forceCloseDate) {
+                if (+new Date(curDate) - +new Date(forceCloseDate) < 7 * oneDayMs) {
+                    return false;
+                } else {
+                    localStorage.removeItem('forceClose');
+                }
+            }
+
+            return true;
         },
 
         show: function() {
@@ -129,6 +163,28 @@
 
             this.el.hide();
         },
+
+        /**
+         * @para dateObj {object} 日期对象
+         * return format date 2014-11-25
+         */
+        getFormatDate: function( dateObj ) {
+            var year = dateObj.getFullYear(),
+                month = dateObj.getMonth() + 1,
+                date = dateObj.getDate();
+
+            function actlt10( num ) {
+                var rst = num;
+
+                if ( num < 10 ) {
+                    rst =  '0' + num;
+                }
+
+                return rst;
+            }
+
+            return year + '-' + actlt10(month) + '-' + actlt10(date);
+        }
     });
 
     return shortcut;
