c9a78ed8ccc2f101f0cc55a064e706b69839891b
zhangchen <zhangchen2397@126.com>
2014-11-26 17:09:11 +0800

    finish shortcut demo

--- zc/shortcut/shortcutAndroid.js
+++ zc/shortcut/shortcutAndroid.js
@@ -1,52 +1,58 @@
 /**
- * describe
+ * 针对android下添加桌面快捷方式
+ * @author samczhang@tencent.com
+ * @date 2014-11-26
+ *
+ * @example
+ *  new shortcutAndroid({
+ *      url: 'http://4g.qq.com',             //apk下载地址
+ *      tips: '添加快捷方式，一键访问手腾',  //提示方案
+ *      key: 'home'                          //业务标识 
+ *  });
  *
  */
 
 (function(root, factory) {
     if (typeof define === 'function') {
-        define('shortcut', ['jqmobi'], function($) {
+        define('shortcutAndroid', ['jqmobi'], function($) {
             return factory(root, $);
         });
     } else {
-        root.shortcut = factory(root, root.$);
+        root.shortcutAndroid = factory(root, root.$);
     }
 })(window, function(root, $) {
 
     var styleStr = [
-        '#shortcut-pannel {display:none; z-index:9999; position:fixed; width:186px; height:76px; bottom:10px; background-color:#666666; opacity:0.9; border-radius:3px; }',
-        '#shortcut-pannel .con { position:relative; height:76px; }',
-        '#shortcut-pannel .ic-bg {background:transparent url(http://3gimg.qq.com/wap30/infoapp/touch/itravel/images/img/bg-ic.png) no-repeat scroll; -webkit-background-size: 20px auto;}',
-        '#shortcut-pannel .ic-closebtn {position:absolute; right:10px; top:10px; z-index:3; width:11px; height:11px; background-position:left top;}',
-        '#shortcut-pannel .tips {padding:15px 10px;}',
-        '#shortcut-pannel .tips img {width:45px; border-radius:3px; float:left; margin-right:10px;}',
-        '#shortcut-pannel .tips p {color:#FFFFFF; position:relative; top:-3px;}',
-        '#shortcut-pannel .ic-add { display:inline-block; width:16px; height:20px; background-position:left -19px; position:relative; top:3px; margin-left:5px; }',
-        '#shortcut-pannel .ic-arrow {position:absolute; width:20px; height:10px; bottom:-10px; left:83px; background-position:left -44px;}'
+        '#shortcut-a-pannel {display:none; z-index:9999; position:fixed; border:1px solid solid rgba(215,215,215,0.9);; width:205px; height:100px; bottom:10px; background-color:#EBEBEB; opacity:0.9; border-radius:3px; }',
+        '#shortcut-a-pannel .con { position:relative; }',
+        '#shortcut-a-pannel .ic-closebtn {position:absolute; right:10px; top:8px; z-index:3; width:11px; height:11px; background:transparent url(http://3gimg.qq.com/wap30/infoapp/touch/itravel/images/img/ic-closebtn.png) no-repeat scroll left top; -webkit-background-size: 11px auto;}',
+        '#shortcut-a-pannel p.tips {text-align:center; color:#4C4C4C; font-size:14px; padding:25px 0 10px;}',
+        '#shortcut-a-pannel p.link {text-align:center;}',
+        '#shortcut-a-pannel p.link a {display:inline-block; border-radius:3px; width:112px; height:32px; line-height:32px; color:#FFFFFF; background-color:#2B73DF; text-align:center; text-decoration:none;}',
     ].join('');
 
     var tplStr = [
-        '<div id="shortcut-pannel">',
+        '<div id="shortcut-a-pannel">',
             '<div class="con">',
-                '<div class="ic-bg ic-closebtn"></div>',
-                '<div class="tips">',
-                    '<img src="{%imgurl%}" />',
-                    '<p>先点击<span class="ic-bg ic-add"></span><br/>再"添加到主屏幕"</p>',
-                '</div>',
-                '<div class="ic-bg ic-arrow"></div>',
+                '<div class="ic-closebtn"></div>',
+                '<p class="tips">{%tips%}</p>',
+                '<p class="link"><a href="{%url%}" target="_blank">好的</a></p>',
             '</div>',
         '</div>'
     ].join('');
 
     var shortcut = function(config) {
         this.defaultConfig = {
-            icon: 'http://3gimg.qq.com/wap30/infoapp/touch/itravel/images/img_travel.png',
-            key: 'travel',
-            url: 'http://meiyou.qq.com',
-            tips: '添加快捷方式，一键访问美游'
+            url: 'http://4g.qq.com',
+            tips: '添加快捷方式，一键访问手腾',
+            key: 'home'
         };
 
         this.config = $.extend({}, this.defaultConfig, config || {});
+
+        //set localStorage key name
+        this.dailyShowKey = 'dailyShow-' + this.config.key;
+        this.forceCloseKey = 'forceClose-' + this.config.key;
 
         this.init.call(this);
     };
@@ -58,7 +64,7 @@
                 this._initEvent();
 
                 //记录显示日期
-                localStorage.setItem('dailyShow', this.getFormatDate(new Date()));
+                localStorage.setItem(this.dailyShowKey, this.getFormatDate(new Date()));
             }
         },
 
@@ -66,12 +72,13 @@
             var me = this,
                 config = this.config;
 
-            tplStr = tplStr.replace( '{%imgurl%}', config.icon );
+            tplStr = tplStr.replace('{%tips%}', config.tips)
+                           .replace('{%url%}', config.url);
 
-            $( 'head' ).append( '<style>' + styleStr + '</style>' );
-            $( document.body ).append( tplStr );
+            $('head').append('<style>' + styleStr + '</style>');
+            $( document.body ).append(tplStr);
 
-            this.el = $('#shortcut-pannel');
+            this.el = $('#shortcut-a-pannel');
 
             this.show();
         },
@@ -82,7 +89,7 @@
                 win = $( window );
 
             this.el.css( {
-                left: (win.width() - 186) / 2 + 'px'
+                left: (win.width() - 205) / 2 + 'px'
             } );
         },
 
@@ -93,7 +100,7 @@
             this.hide();
 
             //记录主动关闭日期
-            localStorage.setItem('forceClose', this.getFormatDate(new Date()));
+            localStorage.setItem(this.forceCloseKey, this.getFormatDate(new Date()));
         },
 
         _initEvent: function() {
@@ -104,6 +111,7 @@
             $(window).on('orientationchange', $.proxy(this._setPos, this));
 
             this.el.find('.ic-closebtn').on('click', $.proxy(this._actClose, this));
+            this.el.find('p.link a').on('click', $.proxy(this._actClose, this));
         },
 
         _autoClose: function() {
@@ -112,20 +120,24 @@
 
             setTimeout(function() {
                 me.hide();
-            }, 4000);
+            }, 6000);
         },
 
         _isShow: function() {
             var me = this,
                 config = this.config,
                 oneDayMs = 1000 * 60 * 60 * 24,
-                ua = navigator.userAgent,
                 curDate = this.getFormatDate(new Date()),
-                lastShowDate = localStorage.getItem('dailyShow'),
-                forceCloseDate = localStorage.getItem('forceClose');
+                lastShowDate = localStorage.getItem(this.dailyShowKey),
+                forceCloseDate = localStorage.getItem(this.forceCloseKey);
 
-            //非ios下的safari不显示
-            if (!/iphone.*?safari/i.test(ua)) {
+            //如不是android下，则不显示
+            if (!this.isAndroid()) {
+                return false;
+            }
+
+            //如是已安装，则不显示
+            if (window.isInstall) {
                 return false;
             }
 
@@ -141,7 +153,7 @@
                 if (+new Date(curDate) - +new Date(forceCloseDate) < 7 * oneDayMs) {
                     return false;
                 } else {
-                    localStorage.removeItem('forceClose');
+                    localStorage.removeItem(this.forceCloseKey);
                 }
             }
 
@@ -154,7 +166,7 @@
 
             this._setPos();
             this.el.show();
-            //this._autoClose();
+            this._autoClose();
         },
 
         hide: function() {
@@ -184,6 +196,20 @@
             }
 
             return year + '-' + actlt10(month) + '-' + actlt10(date);
+        },
+
+        isAndroid: function() {
+            var ua = navigator.userAgent;
+
+            //android系统, 排除uc
+            if (/android/i.test(ua)) {
+                //排除uc
+                if (!/ucbrowser/i.test(ua)) {
+                    return true;
+                }
+            }
+
+            return false;
         }
     });
 
